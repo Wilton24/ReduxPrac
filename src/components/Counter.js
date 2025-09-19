e44d22bf4f3a254224f3b4e710a121890647cce5
Wilton Batiquin <cbasher24@gmail.com>
2025-09-19 17:35:54 +0800

    adding counter and provider

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'TOGGLE_COUNTER' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
